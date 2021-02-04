3d12147fa67cd7f7953cd968c7087aa3085223a4
prpeilun <60053731+prpeilun@users.noreply.github.com>
2021-02-04 15:09:34 +0800

    Add files via upload

--- js/tool.js
+++ js/tool.js
@@ -111,4 +111,21 @@ function fanyi(ch) {
   return en
 }
 
-console.log(new Date() > new Date(2020, 11, 13, 11, 3, 0))
+function shuffleSelf(array, size) {
+    var index = -1,
+        length = array.length,
+        lastIndex = length - 1;
+
+    size = size === undefined ? length : size;
+    while (++index < size) {
+        // var rand = baseRandom(index, lastIndex),
+        var rand = index + Math.floor( Math.random() * (lastIndex - index + 1))
+            value = array[rand];
+
+        array[rand] = array[index];
+
+        array[index] = value;
+    }
+    array.length = size;
+    return array;
+}
